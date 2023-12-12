cd2d3d4df73b8882dff193fb96e5b37a2ec4e360
Surefire10 <109690531+Surefire10@users.noreply.github.com>
2023-12-12 23:47:38 +0200

    Added content

---
+++ app/components/compose/page.tsx
@@ -0,0 +1,100 @@
+
+"use client"
+import { useDisclosure } from "@mantine/hooks";
+import { useState } from 'react';
+import { Container, Anchor, Group, Burger, Box, Text, Title } from '@mantine/core';
+import classes from './home.module.css';
+import { Home, User, BookOpen } from "react-feather";
+
+const links = [
+
+    {link:"./home", label: "Home" ,icon: <Home className={classes.icon}/>},
+    {link:"./user", label: "My Profile",icon: <User className={classes.icon}/>},
+    {link:"./compose", label: "New Recipe",icon: <BookOpen className={classes.icon}/>},
+
+]
+
+
+function Header(){
+
+    return(
+
+        <header className={classes.header}>
+                <Box className={classes.title}>
+                    <Title fw= {700} order={1}>CookMate</Title>
+                </Box>
+        </header>
+
+    )
+}
+
+function Navbar(){
+
+    const[active , setActive] = useState("Home")
+    console.log(active)
+    const items = links.map((item) =>{
+
+       return( 
+        <a 
+        href={item.link}
+        key={item.label}
+        className={classes["nav-link"]}
+        data-active={item.label === active || undefined}
+        onClick={(event) =>{
+            event.preventDefault();
+            setActive(item.label)
+        }}
+        >
+           {item.icon} {item.label}
+        </a>
+       )
+    })
+    
+
+return(
+
+    <nav className={classes.navbar}>
+        <div className={classes.main}>
+            {items}
+        </div>
+        <div className={classes.footer}>
+            foooot
+        </div>
+    </nav>
+
+)
+
+
+}
+
+
+function MainArea(){
+
+
+    return(
+
+        <Container className={classes["main-area"]}>
+                <div className={classes["recipe-container"]}>
+
+                </div>
+        </Container>
+    )
+
+
+}
+
+
+
+
+export default function HomePage() {
+
+    return(
+        <>
+        <Header/>
+        <div className={classes.container}>
+            <Navbar/>
+            <MainArea/>
+        </div>
+        </>
+    )
+}
